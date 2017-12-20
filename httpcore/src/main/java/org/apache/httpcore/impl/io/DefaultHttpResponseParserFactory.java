c51045ea11690faa3fc1a2d5b9f3165281094d85
YanZhenjie <smallajax@foxmail.com>
2017-12-20 14:29:54 +0800

    Add apache-httpcore component.

---
+++ httpcore/src/main/java/org/apache/httpcore/impl/io/DefaultHttpResponseParserFactory.java
@@ -0,0 +1,73 @@
+/*
+ * ====================================================================
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+
+package org.apache.httpcore.impl.io;
+
+import org.apache.httpcore.HttpResponse;
+import org.apache.httpcore.HttpResponseFactory;
+import org.apache.httpcore.annotation.ThreadingBehavior;
+import org.apache.httpcore.annotation.Contract;
+import org.apache.httpcore.config.MessageConstraints;
+import org.apache.httpcore.impl.DefaultHttpResponseFactory;
+import org.apache.httpcore.io.HttpMessageParser;
+import org.apache.httpcore.io.HttpMessageParserFactory;
+import org.apache.httpcore.io.SessionInputBuffer;
+import org.apache.httpcore.message.BasicLineParser;
+import org.apache.httpcore.message.LineParser;
+
+/**
+ * Default factory for response message parsers.
+ *
+ * @since 4.3
+ */
+@Contract(threading = ThreadingBehavior.IMMUTABLE_CONDITIONAL)
+public class DefaultHttpResponseParserFactory implements HttpMessageParserFactory<HttpResponse> {
+
+    public static final DefaultHttpResponseParserFactory INSTANCE = new DefaultHttpResponseParserFactory();
+
+    private final LineParser lineParser;
+    private final HttpResponseFactory responseFactory;
+
+    public DefaultHttpResponseParserFactory(final LineParser lineParser,
+            final HttpResponseFactory responseFactory) {
+        super();
+        this.lineParser = lineParser != null ? lineParser : BasicLineParser.INSTANCE;
+        this.responseFactory = responseFactory != null ? responseFactory
+                : DefaultHttpResponseFactory.INSTANCE;
+    }
+
+    public DefaultHttpResponseParserFactory() {
+        this(null, null);
+    }
+
+    @Override
+    public HttpMessageParser<HttpResponse> create(final SessionInputBuffer buffer,
+            final MessageConstraints constraints) {
+        return new DefaultHttpResponseParser(buffer, lineParser, responseFactory, constraints);
+    }
+
+}
